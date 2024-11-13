b9c22aaf4f8ea8a066d312e555227b93ec6d49c8
JeongTaek <jtkim965@naver.com>
2024-11-13 23:23:28 +0900

    Setting : package.json 수정

--- db.js
+++ db.js
@@ -1,18 +1,18 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2')
 
 const connection = mysql.createConnection({
-  host: 'localhost',        
-  user: 'root',             
-  password: '1234',        
-  database: 'stockey' 
-});
+  host: 'localhost',
+  user: 'root',
+  password: '1234',
+  database: 'stockey',
+})
 
-connection.connect((err) => {
+connection.connect(err => {
   if (err) {
-    console.error('MySQL 연결 실패 : ', err);
-    return;
+    console.error('MySQL 연결 실패 : ', err)
+    return
   }
-  console.log('데이터베이스에 연결되엇습니다');
-});
+  console.log('데이터베이스에 연결되엇습니다')
+})
 
-module.exports = connection;
+module.exports = connection
