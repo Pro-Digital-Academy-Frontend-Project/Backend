261a0edebae207ccfa19cef75aa3886fd7f07a3e
JeongTaek <jtkim965@naver.com>
2024-11-13 22:30:12 +0900

    Setting : DB 연결 및 패키지 설치

---
+++ db.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: 'localhost',        
+  user: 'root',             
+  password: '1234',        
+  database: 'stockey' 
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('MySQL 연결 실패 : ', err);
+    return;
+  }
+  console.log('데이터베이스에 연결되엇습니다');
+});
+
+module.exports = connection;
