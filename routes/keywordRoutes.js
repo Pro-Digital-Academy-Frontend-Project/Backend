87ce40702bea8792f54884b11ad3e79f1cad03d7
JeongTaek <jtkim965@naver.com>
2024-11-18 20:49:19 +0900

    #11 Refactor : API 경로 오타 수정

--- routes/keywordRoutes.js
+++ routes/keywordRoutes.js
@@ -4,6 +4,6 @@ const keywordController = require('../controllers/keywordController')
 
 router.get('/stocks/:stock_id', keywordController.keywordRankingByStock);
 router.get('/:keyword_id/stock-rankings', keywordController.stocksRankingByKeyword);
-router.get('/total-ranking', keywordController.totalRanking)
+router.get('/total-rankings', keywordController.totalRanking)
 
 module.exports = router
