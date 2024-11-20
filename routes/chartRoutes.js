8beae7bf876c4a42077d947f9e54aea2288e16fb
kwak <sal091625@gmail.com>
2024-11-20 17:16:10 +0900

    fix(#10): stock chart data get route

--- routes/chartRoutes.js
+++ routes/chartRoutes.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const router = express.Router();
-const chartController = require('../controllers/chartController');
+const express = require('express')
+const router = express.Router()
+const chartController = require('../controllers/chartController')
 
 // GET 특정 차트 데이터 조회
-router.get('/chart',chartController.getChart);
+router.get('/chart/:stock_code', chartController.getChart)
 
 module.exports = router
