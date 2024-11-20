2f1f1575b76671aed43ac97e680ef04e2d921c32
kwak <sal091625@gmail.com>
2024-11-20 16:31:49 +0900

    feat: 경로 수정

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
 app.use('/api/chat', chatRouter)
-app.use('/keyword', keywordRouter)
+app.use('/keywords', keywordRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
