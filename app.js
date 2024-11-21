600e0db6ce0b31061ed7e6448a3033d2fc1aaf9f
이나민 <nm0922@catholic.ac.kr>
2024-11-21 10:52:00 +0900

    [Feat] 주식 종목 검색 api 개발

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ var indexRouter = require('./routes/index')
 var chatRouter = require('./routes/chat')
 var usersRouter = require('./routes/userRoutes')
 const keywordRouter = require('./routes/keywordRoutes')
-const chartRouter = require('./routes/chartRoutes')
+const stockRouter = require('./routes/stockRoutes')
 
 var app = express()
 
@@ -62,7 +62,7 @@ app.use('/', indexRouter)
 app.use('/users', usersRouter)
 app.use('/api/chat', chatRouter)
 app.use('/keywords', keywordRouter)
-app.use('/stocks', chartRouter)
+app.use('/stocks', stockRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/stockRoutes.js
+++ routes/stockRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const chartController = require('../controllers/chartController')
+const searchController = require('../controllers/searchController')
 
 // GET 특정 차트 데이터 조회
 router.get('/chart/:stock_code', chartController.getChart)
+router.get('/chart', searchController.searchStockByName);
 
 module.exports = router
