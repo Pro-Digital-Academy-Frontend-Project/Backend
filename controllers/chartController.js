7ae485f4962e3b9397d77e00ab03cca07ad71331
JeongTaek <jtkim965@naver.com>
2025-07-20 02:00:33 +0900

    Refactor: 일봉, 주봉, 월봉에 따른 TTL 재설정

--- controllers/chartController.js
+++ controllers/chartController.js
@@ -19,48 +19,19 @@ function getDateRange() {
   }
 }
 
-// exports.getChart = async (req, res) => {
-//   const { stock_code, chart_period } = req.params  
-//   try {
-//     const { start, end } = getDateRange()
-//     const baseUrl = `https://openapivts.koreainvestment.com:29443/uapi/domestic-stock/v1/quotations/inquire-daily-itemchartprice?FID_COND_MRKT_DIV_CODE=J&FID_INPUT_ISCD=${stock_code}&FID_INPUT_DATE_1=${start}&FID_INPUT_DATE_2=${end}&FID_PERIOD_DIV_CODE=${chart_period}&FID_ORG_ADJ_PRC=0`
-//     const token = getToken()
-
-//     const headers = {
-//       'Content-Type': 'application/json; charset=utf-8',
-//       Authorization: `Bearer ${token}`,
-//       appkey: process.env.APP_KEY,
-//       appsecret: process.env.APP_SECRET,
-//       tr_id: 'FHKST03010100',
-//       custtype: 'P',
-//     }
-
-//     const resp = await axios.get(baseUrl, {
-//       headers: headers,
-//     })
-
-//     const data = resp.data
-//     if (!data) {
-//       return res.status(500).json({ error: '데이터 조회에 실패했습니다.' })
-//     }
-
-//     const stockData = data.output2
-//       .map(item => ({
-//         date: `${item.stck_bsop_date.slice(0, 4)}-${item.stck_bsop_date.slice(4, 6)}-${item.stck_bsop_date.slice(6, 8)}`,
-//         open: parseFloat(item.stck_oprc),
-//         high: parseFloat(item.stck_hgpr),
-//         low: parseFloat(item.stck_lwpr),
-//         close: parseFloat(item.stck_clpr),
-//         volume: parseInt(item.acml_vol),
-//       }))
-//       .sort((a, b) => new Date(a.date) - new Date(b.date))
-
-//     res.json(stockData)
-//   } catch (error) {
-//     console.error('API 호출 중 오류 발생:', error)
-//     res.status(500).json({ error: 'API 호출 중 오류가 발생했습니다.' })
-//   }
-// }
+// TTL 설정 함수
+function getTTLByPeriod(chartPeriod) {
+  switch (chartPeriod) {
+    case 'D': // 일봉 : 하루
+      return 24 * 60 * 60 * 1000;
+    case 'W': // 주봉 : 일주일
+      return 7 * 24 * 60 * 60 * 1000;
+    case 'M': // 월봉 : 한달
+      return 30 * 24 * 60 * 60 * 1000;
+    default: // 기본값 : 하루
+      return 24 * 60 * 60 * 1000;
+  }
+}
 
 // 캐싱 + 백오프 알고리즘 (딜레이를 늘려가며 재시도) But, 해당 코드에서는 딜레이를 늘려가며 시도하지는 않음.
 exports.getChart = async (req, res) => {
@@ -103,9 +74,10 @@ exports.getChart = async (req, res) => {
         }))
         .sort((a, b) => new Date(a.date) - new Date(b.date));
 
-      // 데이터 캐싱
+      // TTL 설정 및 데이터 캐싱
+      const ttl = getTTLByPeriod(chart_period);
       cache.set(cacheKey, stockData);
-      setTimeout(() => cache.delete(cacheKey), 30 * 60 * 1000); // 30분 후 캐시 삭제
+      setTimeout(() => cache.delete(cacheKey), ttl); // 30분 후 캐시 삭제  
 
       console.log('API 데이터 반환 및 캐싱');
       return stockData;
